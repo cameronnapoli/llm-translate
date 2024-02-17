ee535eae52f3176e1684a60dca34ac7b769e137c
cameronnapoli <cameronnapoli@gmail.com>
2024-02-17 08:04:33 -0800

    Add JSDocs

--- src/openai.js
+++ src/openai.js
@@ -2,34 +2,48 @@ const {
   codeToLanguage,
 } = require('./constants.js');
 
+const createPrompt = (text, source, target) => {
+  const sourceLanguage = codeToLanguage[source];
+  const targetLanguage = codeToLanguage[target];
+  if (!sourceLanguage) {
+    throw new Error(`Invalid source language code: ${source}`);
+  }
+  if (!targetLanguage) {
+    throw new Error(`Invalid target language code: ${target}`);
+  }
+
+  return `Translate from ${sourceLanguage} to ${targetLanguage}:\n${text}\n\nTranslation:`;
+}
+
+/**
+ * A class for translating text using the OpenAI API. https://platform.openai.com/docs/api-reference/chat/create
+ */
 class OpenAITranslator {
+  /**
+   * Creates an instance of the class.
+   * @constructor
+   * @param {Object} options - The configuration options for the instance.
+   * @param {string} options.apiKey - The API key to be used for requests.
+   */
   constructor(options) {
     this.apiKey = options.apiKey;
   }
 
-  createPrompt(text, source, target) {
-    const sourceLanguage = codeToLanguage[source];
-    const targetLanguage = codeToLanguage[target];
-    if (!sourceLanguage) {
-      throw new Error(`Invalid source language code: ${source}`);
-    }
-    if (!targetLanguage) {
-      throw new Error(`Invalid target language code: ${target}`);
-    }
-
-    return `Translate from ${sourceLanguage} to ${targetLanguage}:\n${text}\n\nTranslation:`;
-  }
-
+  /**
+   * Translates the given text from the source language to the target language.
+   * @param {string} text - The text to be translated.
+   * @param {string} source - The language code of the source language.
+   * @param {string} target - The language code of the target language.
+   * @param {Object.<string, *>} options - Configuration options to be passed to OpenAI (e.g. model, temperature, etc.): https://platform.openai.com/docs/api-reference/chat/create
+   * @returns {Promise<string>} - The translated text.
+   */
   async translate(text, source, target, options) {
     if (!this.apiKey) {
       throw new Error('API key is required');
     }
     const model = options?.model || 'gpt-3.5-turbo';
-    const temperature = options?.temperature;
-    const max_tokens = options?.max_tokens || undefined;
-    const top_p = options?.top_p || undefined;
 
-    const prompt = this.createPrompt(text, source, target);
+    const prompt = createPrompt(text, source, target);
     
     const headers = {
       'Content-Type': 'application/json',
@@ -38,9 +52,7 @@ class OpenAITranslator {
     const body = {
       model,
       messages: [{ role: 'system', content: prompt }],
-      ...(temperature && { temperature }),
-      ...(max_tokens && { max_tokens }),
-      ...(top_p && { top_p }),
+      ...(options || {}),
     };
     
     const response = await fetch(
